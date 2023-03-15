892d85b54312e103bf96a8a680933060527f1990
Luan Bianchini <luanbianchine1@gmail.com>
2023-03-15 13:22:02 -0300

    feat: implements depeendency injection and factory

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,43 +1,49 @@
 import { Request, Response } from "express";
-import { UserDatabase } from "../database/UserDatabase";
+import { IUserDatabase } from "../interfaces/Interfaces";
 
 export class UserController{
+  private userDatabase: IUserDatabase
 
-  static async users(req:Request, res:Response) {
+  constructor(userDatabase:IUserDatabase) {
+    this.userDatabase = userDatabase
+  }
+
+  
+  async users(req:Request, res:Response) {
     try {
-      const result = await UserDatabase.listUsers()
+      const result = await this.userDatabase.listUsers()
       res.status(200).json({result})
     } catch (error) {
       res.status(404).json({message: 'not found'})
     }
   }
 
-  static async newUser(req:Request, res:Response) {
+  async newUser(req:Request, res:Response) {
     try {
       const {name, role} = req.body
       const age = +req.body.age
-      await UserDatabase.InsertUser({name, age, role})
+      await this.userDatabase.InsertUser({name, age, role})
       res.status(202).json({message: 'created'})
     } catch (error) {
       res.status(400).json(new Error(error))
     }
   }
 
-  static async updateUser(req: Request, res: Response) {
+  async updateUser(req: Request, res: Response) {
     try {
       const {age, name, role} = req.body
       const id = req.params.id
-      await UserDatabase.updateUser(id, {name, age, role})
+      await this.userDatabase.updateUser(id, {name, age, role})
       res.status(200).json({message: 'ok'})
     } catch (error) {
       res.status(400).json(new Error(error))
     }
   }
 
-  static async deleteUser (req: Request, res: Response) {
+  async deleteUser (req: Request, res: Response) {
     try {
       const id = +req.params.id
-      await UserDatabase.deleteUser(id)
+      await this.userDatabase.deleteUser(id)
       res.status(200).json({message: 'ok'})
     } catch (error) {
       res.status(400).json(new Error(error))
