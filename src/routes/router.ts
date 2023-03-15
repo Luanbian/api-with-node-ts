c39042a94bafc51d216044654489b94af886b1c4
Luan Bianchini <luanbianchine1@gmail.com>
2023-03-15 13:54:57 -0300

    fix: not found in routes

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -5,9 +5,9 @@ const router: Router = Router()
 const userController = makeUserController()
 
 //Routes
-router.post("/users", userController.newUser);
-router.get("/users", userController.users);
-router.put("/users/:id", userController.updateUser);
-router.delete("/users/:id", userController.deleteUser);
+router.post("/users", (req, res) => userController.newUser(req, res));
+router.get("/users", (req, res) => userController.users(req, res));
+router.put("/users/:id", (req, res) => userController.updateUser(req, res));
+router.delete("/users/:id", (req, res) => userController.deleteUser(req, res));
 
 export { router };
